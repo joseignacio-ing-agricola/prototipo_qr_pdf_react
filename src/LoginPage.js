972334d5f35ed6a8947f12971d3c802a20b80f3c
Rodrigomontenegrofarias <rodrigo.montenegro@alumnos.uv.cl>
2024-03-04 00:58:42 -0300

    eliminacion de css, de sobra, ademas de modificacion de css styles para nuestro login, botones pequeños de iconos.

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useAuth } from './AuthContext';
 import { useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import Brightness4Icon from '@mui/icons-material/Brightness4'; // Icono para cambiar el tema
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 import './styles.css';
+import logo from './logo.png'; // Importa tu imagen de logo
 
 const LoginPage = () => {
   const { login } = useAuth();
@@ -13,8 +14,33 @@ const LoginPage = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
-  const [theme, setTheme] = useState('light'); // Estado para el tema
-  const [fontSize, setFontSize] = useState(16); // Estado para el tamaño de la letra
+  const [theme, setTheme] = useState('light');
+  const [fontSize, setFontSize] = useState(16);
+  const [colorIndex, setColorIndex] = useState(0);
+  const rainbowColors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'];
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+    if (storedUsername && storedPassword) {
+      setUsername(storedUsername);
+      setPassword(storedPassword);
+    }
+    if (storedUsername) {
+      focusPassword();
+    }
+  }, []);
+
+  useEffect(() => {
+    // Si se cargó un usuario, también actualizamos el estado de la contraseña
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+    if (storedUsername && storedPassword) {
+      setPassword(storedPassword);
+    }
+  }, [username]); // Se activa cuando el estado de 'username' cambia
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -44,55 +70,86 @@ const LoginPage = () => {
   };
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light'); // Cambiar entre temas
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
 
+  const MIN_FONT_SIZE = 10;
+  const MAX_FONT_SIZE = 18;
+  
   const increaseFontSize = () => {
-    setFontSize((prevSize) => prevSize + 2); // Aumentar el tamaño de la letra
+    if (fontSize < MAX_FONT_SIZE) {
+      setFontSize((prevSize) => prevSize + 1);
+    }
+    if (fontSize === MAX_FONT_SIZE - 1) {
+      setColorIndex((prevIndex) => (prevIndex + 1) % rainbowColors.length);
+    }
+  };
+  
+  const decreaseFontSize = () => {
+    if (fontSize > MIN_FONT_SIZE) {
+      setFontSize((prevSize) => prevSize - 1);
+    }
+    if (fontSize === MIN_FONT_SIZE + 1) {
+      setColorIndex((prevIndex) => (prevIndex + 1) % rainbowColors.length);
+    }
   };
 
-  const decreaseFontSize = () => {
-    setFontSize((prevSize) => prevSize - 2); // Disminuir el tamaño de la letra
+  const focusPassword = () => {
+    if (passwordRef.current) {
+      passwordRef.current.focus();
+    }
   };
 
   return (
     <div className={`centered-content ${theme === 'dark' ? 'dark-theme' : ''}`} style={{ fontSize: `${fontSize}px` }}>
-      <div className="login-container">
-      <div className="login-controls-container">
-          <div className="control-box" onClick={toggleTheme}>
-            <Brightness4Icon /> {/* Icono para cambiar el tema */}
+      <div className="login-container" style={{ fontSize: 'inherit' }}>
+        <div className="login-controls-container">
+          <div className="logo-controls-container">
+            <div className="control-box" onClick={increaseFontSize}>
+              A+
+            </div>
+            <div className="control-box" onClick={decreaseFontSize}>
+              A-
+            </div>
+            <div className="control-box" onClick={toggleTheme}>
+              <Brightness4Icon />
+            </div>
           </div>
-          <div className="control-box" onClick={increaseFontSize}>
-            A+ {/* Botón para aumentar el tamaño de la letra */}
-          </div>
-          <div className="control-box" onClick={decreaseFontSize}>
-            A- {/* Botón para reducir el tamaño de la letra */}
+          <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo" />
           </div>
         </div>
-        <h2>Iniciar sesión</h2>
+        <h2 style={{ color: '#000', WebkitTextStroke: `1px ${rainbowColors[colorIndex]}`, textShadow: 'none' }}>Iniciar sesión</h2>
         <form onSubmit={handleLogin}>
-          <Box sx={{ '& .MuiTextField-root': { marginBottom: 1 } }}>
+          <Box sx={{ '& .MuiTextField-root': { marginBottom: '20px', textAlign: 'center' } }}>
             <TextField
-              label="Nombre de usuario"
+              label="Usuario"
               type="text"
-              value={username}
+              defaultValue={username} // Cambiamos value por defaultValue
               onChange={(e) => setUsername(e.target.value)}
               fullWidth
+              className={username ? 'filled' : ''}
+              inputRef={usernameRef}
+              style={{ fontSize: 'inherit' }}
+              InputLabelProps={{ style: { fontSize: 'inherit' } }}
             />
             <TextField
               label="Contraseña"
               type="password"
-              value={password}
+              defaultValue={password} // Cambiamos value por defaultValue
               onChange={(e) => setPassword(e.target.value)}
               fullWidth
+              className={password ? 'filled' : ''}
+              inputRef={passwordRef}
+              style={{ fontSize: 'inherit' }}
+              InputLabelProps={{ style: { fontSize: 'inherit' } }}
             />
           </Box>
-          <Button type="submit" variant="contained" color="primary" fullWidth>
+          <Button type="submit" variant="contained" color="primary" fullWidth style={{ fontSize: 'inherit' }}>
             Iniciar sesión
           </Button>
-          {error && <p className="error-message">{error}</p>}
+          {error && <p className="error-message" style={{ fontSize: 'inherit' }}>{error}</p>}
         </form>
-        
       </div>
     </div>
   );
--- src/QRCodePage.js
+++ src/QRCodePage.js
@@ -1,48 +1,38 @@
-// Importamos la biblioteca React y el componente QRCode de 'qrcode.react'
 import React from 'react';
 import QRCode from 'qrcode.react';
 
-// Definimos un componente funcional llamado QRCodePage
 const QRCodePage = () => {
-  // Definimos la URL que se codificará en el código QR
   const catalogUrl = 'http://localhost:3000/pdf';
 
-  // El componente devuelve un fragmento de JSX
   return (
     <div className="QRCodePage">
-      {/* Contenedor principal */}
-      <div className="centered-content">
-        {/* Contenedor del código QR */}
-        <div>
-          {/* Generamos el código QR con la URL especificada */}
-          <QRCode value={catalogUrl} size={420} level={'H'} includeMargin={true} />
-        </div>
-        {/* Contenedor del botón de descarga */}
-        <div>
-          {/* Botón que al hacer clic descarga el código QR como imagen */}
-          <button
-            onClick={() => {
-              // Seleccionamos el elemento canvas que contiene el código QR
-              const canvas = document.querySelector("canvas");
-              // Convertimos el canvas en una imagen PNG
-              const img = canvas.toDataURL("image/png");
-              // Creamos un enlace de descarga
-              const link = document.createElement('a');
-              // Especificamos el nombre del archivo descargado
-              link.download = 'qr-code.png';
-              // Establecemos el enlace del archivo como la imagen generada
-              link.href = img;
-              // Simulamos un clic en el enlace para iniciar la descarga
-              link.click();
-            }}
-          >
-            Descargar QR como imagen
-          </button>
-        </div>
+      <div className="qr-container" id="qr-container">
+        <QRCode 
+          value={catalogUrl} 
+          level={'H'} 
+          includeMargin={true} 
+          size={256} // Tamaño deseado de la imagen del código QR
+          style={{ width: '256px', height: '256px' }} // Estilo adicional si es necesario
+        />
+      </div>
+
+      <div className="download-button-container">
+        <button
+          className="download-button"
+          onClick={() => {
+            const canvas = document.querySelector("canvas");
+            const img = canvas.toDataURL("image/png");
+            const link = document.createElement('a');
+            link.download = 'qr-code.png';
+            link.href = img;
+            link.click();
+          }}>
+          Descargar QR como imagen
+        </button>
       </div>
     </div>
   );
 };
 
-// Exportamos el componente QRCodePage
 export default QRCodePage;
+
